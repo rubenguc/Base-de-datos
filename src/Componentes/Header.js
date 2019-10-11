acbae2ac1f75781d2228b96329229cda794d8721
Ruben Gutierrez <rubdeivis@gmail.com>
2019-10-10 23:55:21 -0300

    header,tabla a function, estilos agregados

--- src/Componentes/Header.js
+++ src/Componentes/Header.js
@@ -1,14 +1,32 @@
-import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
 
+const useStyles = makeStyles(theme => ({
+  toolbarTitle: {
+    flex: 1,
+    align: 'center'
+  }
+}));
 
-export default props =>
-<AppBar position="static">
-        <Toolbar>
-        <Typography variant="headline"  color="inherit">
-        Sistema de Garantias
+export default function Header(props) {
+  const classes = useStyles();
+  return (
+    <AppBar position="static">
+      <Toolbar >
+        <Typography
+          component="h2"
+          variant="h5"
+          color="inherit"
+          align="center"
+          noWrap
+          className={classes.toolbarTitle}
+        >
+          Sistema de Garantias
         </Typography>
-        </Toolbar>
-</AppBar>
+      </Toolbar>
+    </AppBar>
+  );
+}
--- src/Componentes/TablaTicket.js
+++ src/Componentes/TablaTicket.js
@@ -7,28 +7,41 @@ import {
   TableRow,
   TableRowColumn
 } from "material-ui/Table";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import { makeStyles } from "@material-ui/core/styles";
 
-const row = (x, i, header) =>
+const useStyles = makeStyles(theme => ({
+  card: {
+    minWidth: 275
+  }
+}));
+
+const row = (x, i, header) => (
   <TableRow key={`tr-${i}`}>
-    {header.map((y, k) =>
-      <TableRowColumn key={`trc-${k}`}>
-        {x[y.prop]}
-      </TableRowColumn>
-    )}
-  </TableRow>;
+    {header.map((y, k) => (
+      <TableRowColumn key={`trc-${k}`}>{x[y.prop]}</TableRowColumn>
+    ))}
+  </TableRow>
+);
 
-export default ({ data, header }) =>
-  <Table>
-    <TableHeader>
-      <TableRow>
-        {header.map((x, i) =>
-          <TableHeaderColumn key={`thc-${i}`}>
-            {x.name}
-          </TableHeaderColumn>
-        )}
-      </TableRow>
-    </TableHeader>
-    <TableBody>
-      {data.map((x, i) => row(x, i, header))}
-    </TableBody>
-  </Table>;
+export default function TablaTicket({ data, header }) {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.card}>
+      <CardContent>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              {header.map((x, i) => (
+                <TableHeaderColumn key={`thc-${i}`}>{x.name}</TableHeaderColumn>
+              ))}
+            </TableRow>
+          </TableHeader>
+          <TableBody>{data.map((x, i) => row(x, i, header))}</TableBody>
+        </Table>
+      </CardContent>
+    </Card>
+  );
+}
